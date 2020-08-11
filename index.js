8e737ab7f06b9641eefce07b70622b10738af484
anne02550 <61195411+anne02550@users.noreply.github.com>
2020-08-11 13:00:25 +0100

    Added view engine

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express()
 const port = 3000
 
 app.use('/public', express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('love Tam!')
+app.engine('handlebars', handlebars())
+app.set('view engine', 'handlebars')
+
+app.get('/login', (req, res) => {
+  res.render('login')
 })
+
+app.get('/results', (req, res) => {
+    res.render('results')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)

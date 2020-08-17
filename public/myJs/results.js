b2d51b36742fddee70ce2e0676dbe2414fc7bb2c
anne02550 <61195411+anne02550@users.noreply.github.com>
2020-08-17 23:33:36 +0100

    making delete button work + adding modal alert

--- public/myJs/results.js
+++ public/myJs/results.js
@@ -1,8 +1,37 @@
 $(document).ready(function() {
+
+  $('#add-employee-button').click(() => {
+    $('#find-employee-form.in').collapse('hide');
+    $('#edit-form.in').collapse('hide');
+
+    $('#add-employee-form').collapse("toggle");
+  });
+
+  $('#find-employee-button').click(() => {
+    $('#add-employee-form.in').collapse('hide');
+    $('#edit-form.in').collapse('hide');
+
+    $('#find-employee-form').collapse('toggle');
+  });
+
+  $('.delete-form').submit(function( event ) {
+    event.preventDefault();
+    const url = this.action;
+
+    $("#delete-confirm-form").attr('action', url)
+    $("#confirm-delete").modal({show:true});
+  });
+  // modal
+  // modal end 
+  
     $(".edit").click((event) => {
         var id = $(event.target).attr('employee');
 
+        $('#add-employee-form.in').collapse('hide');
+        $('#find-employee-form.in').collapse('hide');
+
         $('#edit-form').collapse('show');
+
         function success(employee){
           $('#edit-jobTitle').val(employee.jobTitle);
           $('#edit-departmentId').val(employee.departmentId);
@@ -18,5 +47,6 @@ $(document).ready(function() {
 
     });
 })
+
 
 

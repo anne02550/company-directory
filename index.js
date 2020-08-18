7efd4c87f5a27fc164ecbe46585077d0c88d21e6
anne02550 <61195411+anne02550@users.noreply.github.com>
2020-08-18 22:58:10 +0100

    Implemented login and logout

--- index.js
+++ index.js
@@ -2,59 +2,109 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const db = require('./source/database');
+const session = require('express-session');
 
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+app.use(session({
+  key: 'user_id',
+  secret: "some-secret",
+  rolling: true,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false},
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use('/public', express.static('public'))
+app.use('/public', express.static('public'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
 
-app.engine('handlebars', handlebars())
-app.set('view engine', 'handlebars')
+// Utils
+const isLoggedIn = (req) => req.session.user;
 
+const authenticate = (req, res, next) => {
+  if(isLoggedIn(req)) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
+
+// Redirrects
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
+app.get('/logout', async (req, res) => {
+  if (isLoggedIn(req)) {
+    res.clearCookie('user_id');
+    await new Promise(resolve => req.session.destroy(() => resolve()));
+  } 
+  res.redirect('/login');
+});
+
+// Pages
 app.get('/login', (req, res) => {
-  res.render('login', {pageName: "login"})
+  if (isLoggedIn(req)) {
+    res.redirect('/results');
+  } else {
+    const showError = req.query.showError === 'true';
+    res.render('login', {pageName: "login", showError});
+  }
 })
 
-app.get('/results', async (req, res) => {
+app.get('/results', authenticate, async (req, res) => {
     const personnel = await db.getPersonnel(req.query);
     const departments = await db.getAllDepartments();
     const locations = await db.getAllLocations();
 
     res.render('results', {pageName: "results", personnel: personnel, departments: departments, locations: locations})
-})
+});
 
-app.get('/get-employee/:id', async (req, res) => {
+// API
+app.get('/get-employee/:id', authenticate, async (req, res) => {
   const id = req.params.id;
   const result = await db.getPersonnel({id});
   res.send(result[0]);
-})
+});
 
-app.post('/add-employee', async (req, res) => {
-  // TODO validate 
+// Form Posts
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(username === 'hello' && password === 'hello') {
+    //success
+    req.session.user = {id: 1, userName: 'admin'};
+    res.redirect('/results');
+  } else {
+    res.redirect('/login?showError=true');
+  }
+});
+
+app.post('/add-employee', authenticate, async (req, res) => {
   await db.addPersonnel(req.body);
   res.redirect('results')
-})
+});
 
-app.post('/update-employee', async (req, res) => {
-  // TODO validate 
+app.post('/update-employee', authenticate, async (req, res) => {
   await db.editPersonnel(req.body);
-
   res.redirect('results')
-})
+});
 
-app.post('/delete-employee/:id', async (req, res) => {
+app.post('/delete-employee/:id', authenticate, async (req, res) => {
   const id = req.params.id;
   db.deletePersonnel(id);
   res.redirect('/results');
-})
+});
 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});

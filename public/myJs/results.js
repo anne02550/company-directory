7a61035d2af40ec34031b5c6d1a5c60da4aa8ef1
anne02550 <61195411+anne02550@users.noreply.github.com>
2020-08-27 13:23:31 +0100

    missing a bracket,  fixed

--- public/myJs/results.js
+++ public/myJs/results.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
   // click on a button to hide others form and show the form of the button
   // add employee button:
@@ -69,23 +71,5 @@ $(document).ready(function () {
           sticky.removeClass("is-sticky");
           stickyWrapper.height('auto');
       }
-  };
-
-  // Find all data-toggle="sticky-onscroll" elements
-  $('[data-toggle="sticky-onscroll"]').each(function () {
-      var sticky = $(this);
-      var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
-      sticky.before(stickyWrapper);
-      sticky.addClass('sticky');
-
-      // Scroll & resize events
-      $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function () {
-          stickyToggle(sticky, stickyWrapper, $(this));
-      });
-
-      // On page load
-      stickyToggle(sticky, stickyWrapper, $(window));
-  });
-});
-
-
+  }
+})
